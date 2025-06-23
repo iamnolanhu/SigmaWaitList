a20c4e92b19d4815bbdd05b3fe67de56bdb49739
CodesSuzy <codessuzy@gmail.com>
2025-06-23 18:41:48 -0400

    Updated Desktop.tsx

--- src/screens/Desktop/Desktop.tsx
+++ src/screens/Desktop/Desktop.tsx
@@ -120,8 +120,7 @@ export const Desktop = (): JSX.Element => {
                 </h1>
 
                 <p className="font-['Space_Mono'] text-[#b7ffab] text-sm sm:text-base lg:text-lg max-w-md mx-auto leading-relaxed opacity-90 drop-shadow-lg">
-                  Be Sigma. Get the latest Sigma news and early access to our
-                  first build
+                POV: you want to start a business but every tutorial means you gotta listen to some based sigma said 'what if we just... did all of it?'
                 </p>
               </div>
 
